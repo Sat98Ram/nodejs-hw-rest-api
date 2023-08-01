cbf3cdbb18a9cd897945cb8096b5b9fdc049d2f4
Iryna <irinkasatanovskaya@gmail.com>
2023-08-01 10:12:24 +0300

    add error helper, router.get and .post, import Joi

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,25 +1,51 @@
 const express = require("express");
+const Joi = require("joi");
+
+const contacts = require("../../models/contacts.js");
+const { HttpError } = require("../../helpers");
 
 const router = express.Router();
 
-router.get("/", async (req, res, next) => {
-  // const result = await
-  res.json({ message: "template message" });
+const addSchema = Joi.object({
+  name: Joi.string().required(),
 });
 
-router.get("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
+router.get("/", async (req, res, next) => {
+  try {
+    const result = await contacts.listContacts();
+    res.status(200).json(result, { message: "get req" });
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.get("/:id", async (req, res, next) => {
+  try {
+    const { id } = req.params;
+    const result = await contacts.getContactById(id);
+    if (!result) {
+      throw HttpError(404, "Not found");
+    }
+    res.status(200).json(result);
+  } catch (error) {
+    next(error);
+  }
 });
 
 router.post("/", async (req, res, next) => {
+  try {
+    const result = await contacts.addContact(req.body);
+    res.status(201).json(result);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
-router.delete("/:contactId", async (req, res, next) => {
-  res.json({ message: "template message" });
-});
-
-router.put("/:contactId", async (req, res, next) => {
+router.put("/:id", async (req, res, next) => {
   res.json({ message: "template message" });
 });
 
