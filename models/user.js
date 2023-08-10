73bd937cc1bd2499561737d0d534dd426c2c8c3d
Iryna <irinkasatanovskaya@gmail.com>
2023-08-10 14:56:43 +0300

    add userSchema and model

---
+++ models/user.js
@@ -0,0 +1,28 @@
+const { Schema, model } = require("mongoose");
+
+const emailRegExp = /^[a-z0-9]+@[a-z]+\.[a-z]{2-3}$/;
+
+const userSchema = new Schema(
+  {
+    password: {
+      type: String,
+      minlength: 8,
+      required: [true, "Set password for user"],
+    },
+    email: {
+      type: String,
+      required: [true, "Email is required"],
+      unique: true,
+      match: emailRegExp,
+    },
+    subscription: {
+      type: String,
+      enum: ["starter", "pro", "business"],
+      default: "starter",
+    },
+    token: String,
+  },
+  { versionKey: false, timestamps: true }
+);
+
+const User = model("user", userSchema);
