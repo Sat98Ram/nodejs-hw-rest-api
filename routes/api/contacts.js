5e415b0cf52dbc0c35f55b3e32b082df6f2c3894
Iryna <irinkasatanovskaya@gmail.com>
2023-08-10 11:41:20 +0300

    remove comments

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -23,36 +23,5 @@ router.patch(
   validateBody(schemas.updateFavoriteSchema),
   ctrl.updateFavorite
 );
-// router.put("/:id", async (req, res, next) => {
-//   try {
-//     const { error } = addSchema.validate(req.body);
-//     if (error) {
-//       throw HttpError(400, error.message);
-//     }
-//     const { id } = req.params;
-//     const result = await contacts.updateById(id, req.body);
-//     if (!result) {
-//       throw HttpError(400, error.message);
-//     }
-//     res.json(result);
-//   } catch (error) {
-//     next(error);
-//   }
-// });
-
-// router.delete("/:id", async (req, res, next) => {
-//   try {
-//     const { id } = req.params;
-//     const result = await contacts.removeContact(id, req.body);
-//     if (!result) {
-//       throw HttpError(400, "Not found");
-//     }
-//     res.json({
-//       message: "Delete success",
-//     });
-//   } catch (error) {
-//     next(error);
-//   }
-// });
 
 module.exports = router;
