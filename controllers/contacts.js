289a05ae2d7f66c891886849fe1e79249e06305b
Iryna <irinkasatanovskaya@gmail.com>
2023-08-14 12:30:13 +0300

    add filtrarion by favorite

--- controllers/contacts.js
+++ controllers/contacts.js
@@ -4,12 +4,16 @@ const { HttpError, ctrlWrapper } = require("../helpers");
 
 const getAll = async (req, res) => {
   const { _id: owner } = req.user;
-  const { page = 1, limit = 20 } = req.query;
+  const { page = 1, limit = 20, favorite } = req.query;
   const skip = (page - 1) * limit;
-  const result = await Contact.find({ owner }, "-createdAt -updatedAt", {
-    skip,
-    limit,
-  }).populate("owner", "name email");
+  const result = await Contact.find(
+    favorite ? { owner, favorite } : { owner },
+    "-createdAt -updatedAt",
+    {
+      skip,
+      limit,
+    }
+  ).populate("owner", "name email");
   res.json(result);
 };
 
